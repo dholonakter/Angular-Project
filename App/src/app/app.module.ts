b468d6bf26f6032cb4968626850c8ada336eba3d
Remy Bos <remy.bos@student.fontys.nl>
2018-09-03 12:02:50 +0200

    added an employee component to the project, will switch do different branch to develop the feature.

--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent
   ],
   imports: [
     BrowserModule
