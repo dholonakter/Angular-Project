5f87fee375e313b876dacdc2dc4dfcb9cb2ca8af
Remy Bos <remy.bos@student.fontys.nl>
2018-09-14 00:01:35 +0200

    Created getters and setters to keep track of when an employee's information was changed, such that they will be either reverted or made definitive. Also changed some of the functions to control the display output and html.

--- src/app/employee.ts
+++ src/app/employee.ts
@@ -3,29 +3,77 @@ export class Employee
     static id: number = 0;
 
     employeeId: number;
-    firstName: string;
-    lastName: string;
-    phone: string;
-    email: string;
+    _firstName: string;
+    _lastName: string;
+    _phone: string;
+    _email: string;
+
+    detailsChanged: boolean;
 
     constructor(firstName: string, lastName: string, phone: string, email: string) 
     {
         // Assign a unique id to the employee.
         this.employeeId = ++Employee.id;
 
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phone = phone;
-        this.email = email;
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this._phone = phone;
+        this._email = email;
+
+        this.detailsChanged = false;
+    }
+
+    get firstName()
+    {
+        return this._firstName;
+    }
+
+    set firstName(firstName: string)
+    {
+        this._firstName = firstName;
+        this.detailsChanged = true;
+    }
+
+    get lastName()
+    {
+        return this._lastName;
+    }
+
+    set lastName(lastName: string)
+    {
+        this._lastName = lastName;
+        this.detailsChanged = true;
+    }
+
+    get phone()
+    {
+        return this._phone;
+    }
+
+    set phone(phone: string)
+    {
+        this._phone = phone;
+        this.detailsChanged = true;
+    }
+
+    get email()
+    {
+        return this._email;
+    }
+
+    set email(email: string)
+    {
+        this._email = email;
+        this.detailsChanged = true;
     }
 
     displayBasicInformation()
     {
-        return `ID: ${ this.employeeId }, ${ this.firstName } ${ this.lastName }`;
+        return `ID: ${ this.employeeId }, ${ this._firstName } ${ this._lastName }`;
     }
 
     displayDetailedInformation()
     {
-        return `${ this.displayBasicInformation() }, phone: ${ this.phone }, e-mail: ${ this.email }`;
+        return `${ this.displayBasicInformation() }, phone: ${ this._phone }, e-mail: ${ this._email }`;
     }
 }
--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -12,6 +12,17 @@ export class EmployeesComponent implements OnInit {
 
   employees = EMPLOYEES;
   selectedEmployee: Employee;
+  
+  // Keep track of the array index of the selected employee.
+  selectedEmployeeArrayIndex: number;
+
+  // Keep track of the initial values of the variables (before user changes)
+  // Such that we do not have to deal with artificially increasing static
+  // employee IDs.
+  selectedEmployeeOriginalFirstName: string;
+  selectedEmployeeOriginalLastName: string;
+  selectedEmployeeOriginalPhone: string;
+  selectedEmployeeOriginalEmail: string;
 
   constructor() { }
 
@@ -20,6 +31,75 @@ export class EmployeesComponent implements OnInit {
 
   onSelect(employee: Employee)
   {
-    this.selectedEmployee = employee;
+    if (this.selectedEmployee != null)
+    {
+      // If changes were made but not commited, revert changes
+      // and set the changed flag back to false.
+      if (this.selectedEmployee.detailsChanged)
+      {
+        this.cancelChanges();
+      }
+    }
+      // Switch to the other employee.
+      this.selectedEmployee = employee;
+
+      // As well as keep track of the array index again.
+      this.selectedEmployeeArrayIndex = this.employees.indexOf(this.selectedEmployee);
+    
+      // Store the information of the selected employee into variables.
+      this.selectedEmployeeOriginalFirstName = this.selectedEmployee.firstName;
+      this.selectedEmployeeOriginalLastName = this.selectedEmployee.lastName;
+      this.selectedEmployeeOriginalPhone = this.selectedEmployee.phone;
+      this.selectedEmployeeOriginalEmail = this.selectedEmployee.email;
+  }
+
+  deleteSelectedEmployee(employee: Employee)
+  {
+    let currentIndex = this.employees.indexOf(this.selectedEmployee);
+
+    // Remove the selected employee from the list.
+    this.employees.splice(currentIndex, 1);
+    
+    // Change selection to being one employee earlier in the array or null if none remains. 
+    if (this.employees.length > 0)
+    {
+      if (currentIndex - 1 < 0)
+      {
+        this.selectedEmployee = this.employees[currentIndex];
+      }
+      else
+      {
+        this.selectedEmployee = this.employees[currentIndex - 1];
+      }
+    }
+    else
+    {
+      this.selectedEmployee = null;
+    }
+  }
+
+  createEmployee()
+  {
+    let emp: Employee = new Employee("First Name", "Last Name", "Phone", "E-mail");
+    this.employees.push(emp);
+
+    this.selectedEmployee = emp;
+  }
+
+  changeEmployeeDetails()
+  {
+    // Set flag back such that details won't be reverted.
+    this.selectedEmployee.detailsChanged = false;
+  }
+
+  cancelChanges()
+  {
+    // Revert changes.
+    this.employees[this.selectedEmployeeArrayIndex].firstName = this.selectedEmployeeOriginalFirstName;
+    this.employees[this.selectedEmployeeArrayIndex].lastName = this.selectedEmployeeOriginalLastName;
+    this.employees[this.selectedEmployeeArrayIndex].phone = this.selectedEmployeeOriginalPhone;
+    this.employees[this.selectedEmployeeArrayIndex].email = this.selectedEmployeeOriginalEmail;
+
+    this.employees[this.selectedEmployeeArrayIndex].detailsChanged = false;
   }
 }
