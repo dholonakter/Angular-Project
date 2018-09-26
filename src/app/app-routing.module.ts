e4cddd54f96a8e3cf191a806abb782a872c3b2f4
Remy Bos <remy.bos@student.fontys.nl>
2018-09-26 18:55:10 +0200

    Added a routing component and routerlinks to respective components while moving the navbar from dashboard to app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { DepartmentComponent } from './department/department.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'tasks', component: TasksComponent },
+  { path: 'employees', component: EmployeesComponent },
+  { path: 'departments', component: DepartmentComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+]
 
 @NgModule({
-  exports: [ RouterModule ]
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(routes) ],
 })
 export class AppRoutingModule { }
