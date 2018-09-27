636b7e32c32b0da3f99972f521a951806215aaae
Dholon Akter <dholonakter721@gmail.com>
2018-09-27 12:38:34 +0200

    trying to fix packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { TasksComponent } from './tasks/tasks.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
-
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { DepartmentComponent } from './department/department.component';
 import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
@@ -15,6 +12,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { TasksDetailComponent } from './tasks-detail/tasks-detail.component';
 import { DepartmentDetailComponent } from './department/department-detail/department-detail.component';
 import { EmployeeFilterPipe } from './employee-filter.pipe';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 
 @NgModule({
@@ -33,8 +32,7 @@ import { EmployeeFilterPipe } from './employee-filter.pipe';
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    NgbModule,
-    NgbModule.forRoot()
+    NgbModule
   ],
   providers: [HttpClientModule],
   bootstrap: [AppComponent]
